55ec260f3908ef483ead27a125b162502e00790f
realmacoy <roboneill15@gmail.com>
2017-02-08 18:52:11 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Rob on 2017-02-08.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
